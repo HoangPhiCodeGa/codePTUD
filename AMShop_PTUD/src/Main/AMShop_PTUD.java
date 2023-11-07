7dabbe38673f53ad9b267144de60c0e27c150d3e
HoangPhiCodeGa <phamhoangphi1303@gmail.com>
2023-11-07 07:37:56 +0700

    update ptud

---
+++ AMShop_PTUD/src/Main/AMShop_PTUD.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Main;
+
+import GUI.frmMain;
+
+/**
+ *
+ * @author NHAT TUNG
+ */
+public class AMShop_PTUD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frmMain guiMain = new frmMain();
+        guiMain.setVisible(true);
+ 
+    }
+    
+}
